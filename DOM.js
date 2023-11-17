3c807844ca05b9a16b098b017247df5bf0710e8f
alumno <alumno@Instituto.local>
2023-11-17 09:33:38 -0300

    cambios en el doc
    queryselectors y stule en js

---
+++ DOM.js
@@ -0,0 +1,13 @@
+let boton = document.querySelector('.btn btn-light dropdown-toggle')
+boton.ondbclick = function(){
+    onclick == true
+    return "lugares.html"
+}
+
+let titulo = document.querySelector("h1")
+titulo.innerText = 'Hola alumnos'
+
+
+let parrafo = document.querySelector("#parrafo")
+//parrafo.style.color = "azul";
+parrafo.style.fontFamily = "serif";
