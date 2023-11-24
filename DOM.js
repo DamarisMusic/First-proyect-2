0ca262d4d1d0732159428a9769b00554601649a1
alumno <alumno@Instituto.local>
2023-11-24 09:22:36 -0300

    changes
    24/10/2023

--- DOM.js
+++ DOM.js
@@ -1,13 +1,13 @@
 let boton = document.querySelector('.btn btn-light dropdown-toggle')
 boton.ondbclick = function(){
     onclick == true
-    return "lugares.html"
+    return 
 }
 
-let titulo = document.querySelector("h1")
+let titulo = document.querySelector("#inicio")
 titulo.innerText = 'Hola alumnos'
 
 
 let parrafo = document.querySelector("#parrafo")
-//parrafo.style.color = "azul";
+parrafo.style.color = "azul";
 parrafo.style.fontFamily = "serif";
