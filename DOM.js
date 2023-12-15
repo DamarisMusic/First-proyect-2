e2493000ffed387881b15bbf9bdaf758d8b8f1e5
alumno <alumno@Instituto.local>
2023-12-15 09:29:30 -0300

    dom
    changes ub the codes

--- DOM.js
+++ DOM.js
@@ -1,13 +1,69 @@
-let boton = document.querySelector('.btn btn-light dropdown-toggle')
-boton.ondbclick = function(){
-    onclick == true
-    return 
+let boton = document.querySelector('#botonImagen')
+boton.onclick = 
+function()
+{
+    alert('Comprar')
+    console.log('Comprar')
+    botonImagen.style.backgroundColor = 'dark'
+    botonImagen.style.fontSize = '50px'
+    botonImagen.style.fontFamily = 'serif'
 }
 
-let titulo = document.querySelector("#inicio")
-titulo.innerText = 'Hola alumnos'
+let titulo = document.querySelector("#empanada")
+titulo.innerText
 
 
 let parrafo = document.querySelector("#parrafo")
-parrafo.style.color = "azul";
+parrafo.style.color = "blue";
 parrafo.style.fontFamily = "serif";
+
+botonImagen = document.querySelector('#botonImagen')
+botonImagen.style.color = 'red'
+
+let cambiarImagen =
+document.querySelector('#imagen')
+cambiarImagen.src = 
+'albahaca.jfif'
+
+let imagen =
+document.querySelector('img')
+
+
+let edadUsuario = Number(prompt('Ingresa tu edad'))
+if (edadUsuario >= 18){
+    console.log('Puedes tomar')
+    return alert('podes tomar')
+}else{
+    return alert('sos muy chiquito, no podes tomar')
+}
+
+
+/*ej*/
+
+let 
+
+if(edadUsuario >= 18){
+    texto.textContent = 'emoji'
+}else{
+    texto.textContent = 'emoji'
+}
+/* pasos para el examen
+1) creamos el archivo html y css
+2) incluimos el archivo js en el html fuera del body
+3) colocar identificadores y clases a los elementos html
+4) a) id llamado con el # (#text)
+   b) classes llamadas con el punto (.text)
+   c) elementos en html llamados su nombre comun (text)
+5) verificar que las variables utilizadas esten ceadas con la palabra reservada de 'let'
+6) verificar q los elementos esten 0bien0 es0critos.
+7) Leer la teoria
+*/
+
+/* function tata() 
+if{
+    condicion
+}else{
+    alert
+    return ...
+}*/
+
